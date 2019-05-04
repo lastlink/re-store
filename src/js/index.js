74f4ee042be0b3e7ffdf0222d5640ff7e1dba38f
lastlink <theefunk@gmail.com>
2019-05-04 11:18:53 -0400

    init mobile

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,17 @@ var app = new Vue({
     el: '#Web_1920___home',
     data: {
         message: 'Hello Vue!',
+        checkedNeeds: [],
         todos: [
             { text: 'Learn JavaScript' },
             { text: 'Learn Vue' },
             { text: 'Build something awesome' }
-          ]
+          ],
+        request: function (){
+            console.log("request")
+        },
+        offer: function (){
+            console.log("request")
+        }
     }
 })
