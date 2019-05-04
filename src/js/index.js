9130999e6b71cd3004a11cc99147ea0982ddc1df
lastlink <theefunk@gmail.com>
2019-05-04 10:35:49 -0400

    update

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#Web_1920___home',
+    data: {
+        message: 'Hello Vue!',
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+          ]
+    }
+})
