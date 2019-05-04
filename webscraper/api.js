35db6a7f71e1625d77c9d9fa4bdb43fe781fac74
lastlink <theefunk@gmail.com>
2019-05-04 16:29:03 -0400

    working on await

--- webscraper/api.js
+++ webscraper/api.js
@@ -6,9 +6,41 @@ app.listen(port, () => {
     console.log("Server running on port http://localhost:3000");
 });
 
-app.get("/scrape", (req, res, next) => {
+const scraper = require("./scraper");
+
+
+app.get("/scrape", async (req, res, next) => {
+    var zoom = req.query.zoom,
+        lat = req.query.lat,
+        long = req.query.long;
+
+    // return scraper(zoom, lat, long).then((resp) =>{
+
+    var resp = await scraper(zoom, lat, long);
+
+    if (resp && !resp.base64Str)
+        console.log(res.status)
+
+    console.log("returning response")
+    // console.log(res)
+
+    if (!resp) {
+        resp = {
+            status: 404
+        }
+    }
+
+    var response = {
+        people: ["Tony", "Lisa", "Michael", "Ginger"],
+        status: resp.status,
+        // req.query
+        base64Str: resp.base64Str
+    }
+
     res.setHeader('Content-Type', 'application/json');
-    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+    res.json(response);
+    // })
+
 });
 
 // console.log('todo list RESTful API server started on: ' + port);
