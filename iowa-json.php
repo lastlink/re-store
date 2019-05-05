d2d0c737deac2536cb42a96987c4fb1302915b04
lastlink <theefunk@gmail.com>
2019-05-05 00:44:22 -0400

    working cors

--- iowa-json.php
+++ iowa-json.php
@@ -3,7 +3,25 @@ $servername = "localhost";
 $username = "username";
 $password = "pw";
 $dbname = "dbname";
+if(isset($_SERVER["HTTP_ORIGIN"]))
+{
+    // You can decide if the origin in $_SERVER['HTTP_ORIGIN'] is something you want to allow, or as we do here, just allow all
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+}
+else
+{
+    //No HTTP_ORIGIN set, so we allow any. You can disallow if needed here
+    header("Access-Control-Allow-Origin: *");
+}
 
+header("Access-Control-Allow-Credentials: true");
+
+header('Content-Type: application/json');
+
+
+header('Access-Control-Allow-Methods: GET');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
--- python/pythonrunner.php
+++ python/pythonrunner.php
@@ -1,8 +1,23 @@
 <?php 
+
+// echo $_SERVER["HTTP_ORIGIN"];
+if(isset($_SERVER["HTTP_ORIGIN"]))
+{
+    // You can decide if the origin in $_SERVER['HTTP_ORIGIN'] is something you want to allow, or as we do here, just allow all
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+}
+else
+{
+    //No HTTP_ORIGIN set, so we allow any. You can disallow if needed here
+    header("Access-Control-Allow-Origin: *");
+}
+
+header("Access-Control-Allow-Credentials: true");
+
 $command = escapeshellcmd('/home/funktech/Python-3.5.2/python ../../python/request_offer_match.py');
 header('Content-Type: application/json');
 $output = shell_exec($command);
-header('Access-Control-Allow-Origin: *');
+
 
 header('Access-Control-Allow-Methods: GET');
 
