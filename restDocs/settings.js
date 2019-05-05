90cea182fe83c3247b5e3ac3a548a295a4bb2270
lastlink <theefunk@gmail.com>
2019-05-05 04:12:26 -0400

    init settings docs

---
+++ restDocs/settings.js
@@ -0,0 +1,17 @@
+// >Preferences: Open Settings (JSON)
+var settings = {
+    "rest-client.environmentVariables": {
+        "smbusweek19-local": {
+            "zipcodeapiToken": "jstoken",
+            "url_oauth": "http://127.0.0.1:8000/oauth.php",
+            "otoken": "oauthtoken",
+            "osecret": "secret"
+        },
+        "smbusweek19-live": {
+            "zipcodeapiToken": "jstoken",
+            "url_oauth": "https://re-store.funktecno.com/sso/oauth.php",
+            "otoken": "oauthtoken",
+            "osecret": "secret"
+        },
+    }
+}
